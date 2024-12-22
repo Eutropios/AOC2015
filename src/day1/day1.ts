a69cb03283cc5be15c16de46024ac9dd106ae138
Noah Jenner <64815328+Eutropios@users.noreply.github.com>
2024-12-21 20:40:44 -0800

    Finished problem (Answer below)
    The answer is 74.
    
    Signed-off-by: 64815328+Eutropios@users.noreply.github.com

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,4 +1,17 @@
 import * as fs from "node:fs";
 
-let data = fs.readFileSync("./inputstring.txt", "utf-8");
-console.log(data);
+const data = fs.readFileSync("./inputstring.txt", "utf-8");
+let upFloors = 0;
+let downFloors = 0;
+for (let chara of data) {
+    if (chara === "(") {
+        upFloors++;
+    }
+    if (chara === ")") {
+        downFloors++;
+    }
+}
+
+console.log(`Ups: ${upFloors}`);
+console.log(`Downs: ${downFloors}`);
+console.log(`Number: ${upFloors - downFloors}`);
